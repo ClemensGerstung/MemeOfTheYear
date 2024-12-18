f391d7cb8f4c09d2ca9b377b24a250e09b0a6d2b
Clemens Gerstung <clemens8496@gmail.com>
2024-12-18 22:12:51 +0100

    add equals to session to find it again

--- Backend.Library/Types/Session.cs
+++ Backend.Library/Types/Session.cs
@@ -5,5 +5,24 @@ namespace MemeOfTheYear.Types
         public String Id { get; set; } = "";
 
         public bool IsAuthenticated { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj is not Session) return false;
+
+            return Equals((Session)obj);
+        }
+
+        public bool Equals(Session session)
+        {
+            return Id == session.Id;
+        }
     }
 }
